8143a7c1f3fd231bb829a7d61cdfd133a12c12ab
Martin Revaj <105362541+martin-re@users.noreply.github.com>
2023-09-13 13:27:36 +0200

    Monitoring (#10)
    * [influxdb] Create influxdb workers

---
+++ misc/python/influxdb/README.md
@@ -0,0 +1,14 @@
+# Conductor InfluxDB workers for Frinx Machine
+
+Conductor workers for InfluxDB timeseries database.
+
+## Include
+
+- Create bucket
+- Read data from bucket
+- Write data from bucket
+
+### Prerequisites
+
+- Python 3.10+ is required to use this package.
+
---
+++ misc/python/influxdb/frinx_worker/__init__.py
@@ -0,0 +1,89 @@
+from frinx.common.type_aliases import DictAny
+from frinx.common.type_aliases import ListAny
+from frinx.common.type_aliases import ListStr
+from frinx.common.worker.service import ServiceWorkersImpl
+from frinx.common.worker.task_def import TaskDefinition
+from frinx.common.worker.task_def import TaskExecutionProperties
+from frinx.common.worker.task_def import TaskInput
+from frinx.common.worker.task_def import TaskOutput
+from frinx.common.worker.task_result import TaskResult
+from frinx.common.worker.worker import WorkerImpl
+
+from . import utils
+
+
+class Influx(ServiceWorkersImpl):
+    
+    class InfluxWriteData(WorkerImpl):
+
+        class ExecutionProperties(TaskExecutionProperties):
+            transform_string_to_json_valid: bool = True
+            exclude_empty_inputs: bool = True
+
+        class WorkerDefinition(TaskDefinition):
+            name: str = 'INFLUX_write_data'
+            description: str = 'Write data do InfluxDB'
+            labels: ListStr = ['INFLUX']
+            timeout_seconds: int = 300
+
+        class WorkerInput(TaskInput):
+            org: str
+            token: str
+            bucket: str
+            measurement: str
+            tags: DictAny
+            fields: DictAny
+
+        class WorkerOutput(TaskOutput):
+            pass
+
+        def execute(self, worker_input: WorkerInput) -> TaskResult[WorkerOutput]:
+            return utils.influx_write_data(self, worker_input)
+
+
+    class InfluxQueryData(WorkerImpl):
+
+        class ExecutionProperties(TaskExecutionProperties):
+            transform_string_to_json_valid: bool = True
+            exclude_empty_inputs: bool = True
+
+        class WorkerDefinition(TaskDefinition):
+            name: str = 'INFLUX_query_data'
+            description: str = 'Query data from InfluxDB'
+            labels: ListStr = ['INFLUX']
+            timeout_seconds: int = 300
+
+        class WorkerInput(TaskInput):
+            org: str
+            token: str
+            query: str
+            format_data: ListStr | None
+
+        class WorkerOutput(TaskOutput):
+            data: ListAny
+
+        def execute(self, worker_input: WorkerInput) -> TaskResult[WorkerOutput]:
+            return utils.influx_query_data(self, worker_input)
+
+
+    class InfluxCreateBucket(WorkerImpl):
+
+        class ExecutionProperties(TaskExecutionProperties):
+            exclude_empty_inputs: bool = True
+
+        class WorkerDefinition(TaskDefinition):
+            name: str = 'INFLUX_create_bucket'
+            description: str = 'Create bucket for organization in InfluxDB'
+            labels: ListStr = ['INFLUX']
+            timeout_seconds: int = 300
+
+        class WorkerInput(TaskInput):
+            org: str
+            token: str
+            bucket: str
+
+        class WorkerOutput(TaskOutput):
+            pass
+
+        def execute(self, worker_input: WorkerInput) -> TaskResult[WorkerOutput]:
+            return utils.influx_create_bucket(self, worker_input)
