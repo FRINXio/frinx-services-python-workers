6d153353c1f446930fe3bebaa75733f54eafbe3f
Jozef Volak <jvolak@frinx.io>
2024-03-22 16:48:40 +0100

    [inventory] Update dependencies and small refactor

--- inventory/python/frinx_worker/inventory/utils.py
+++ inventory/python/frinx_worker/inventory/utils.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from typing import Any
-from typing import Optional
 from typing import TypeAlias
 
 import requests
@@ -20,9 +19,7 @@ class InventoryOutput:
 
 
 def execute_inventory_query(
-    query: str,
-    variables: Optional[DictAny] = None,
-    inventory_url_base: str = INVENTORY_URL_BASE
+    query: str, variables: DictAny | None = None, inventory_url_base: str = INVENTORY_URL_BASE
 ) -> InventoryOutput:
     """
     Execute GraphQL query to fetch data from inventory
@@ -33,19 +30,17 @@ def execute_inventory_query(
 
     Returns:
         Http response.
-    """''
+    """ ""
     response = requests.post(
-        inventory_url_base,
-        json={'query': query, 'variables': variables},
-        headers=INVENTORY_HEADERS
+        inventory_url_base, json={"query": query, "variables": variables}, headers=INVENTORY_HEADERS
     )
 
     data = response.json()
 
-    if data.get('errors') is not None:
-        return InventoryOutput(data=data['errors'], status='errors', code=404)
+    if data.get("errors") is not None:
+        return InventoryOutput(data=data["errors"], status="errors", code=404)
 
-    if data.get('data') is not None:
-        return InventoryOutput(data=data['data'], status='data', code=200)
+    if data.get("data") is not None:
+        return InventoryOutput(data=data["data"], status="data", code=200)
 
-    return InventoryOutput(data='Request failed', status='failed', code=500)
+    return InventoryOutput(data="Request failed", status="failed", code=500)
